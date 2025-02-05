2d3831b1e939e9c2937a39c3164a7791a257f91d
ArtRosh <artem.rosh17@gmail.com>
2025-02-04 19:16:43 -0800

    phase-1-second-lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this field
+let currentUser = 'Artem-Rosh';
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`
